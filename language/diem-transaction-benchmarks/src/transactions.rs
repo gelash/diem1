1fbc27970a027b8ba1336175723acb0af8e77d2f
Alexander Spiegelman <sashaspiegelman@fb.com>
2021-11-11 16:17:43 -0800

    minor

--- language/diem-transaction-benchmarks/src/transactions.rs
+++ language/diem-transaction-benchmarks/src/transactions.rs
@@ -113,7 +113,7 @@ where
                 println!("warmup - ignore reults");
                 state.execute();
             } else {
-                println!("Preparing bencher for: block_soze = {}, num_account = {}, write_rate = {}", num_txn, num_accounts, write_keep_rate);
+                println!("Preparing bencher for: num_threads = {}, block_soze = {}, num_account = {}, write_rate = {}", num_cpus::get(), num_txn, num_accounts, write_keep_rate);
                 ret.push(state.execute());
             }
         }
